e40ae637998eaf8d62f3ed4ac45884304ffadac9
horatio-mulholland <horatiomul@gmail.com>
2025-01-19 13:02:28 +0000

    Created getComputerChoice() function

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log("Hello, World!");
+function GetComputerChoice() {
+    const rand_num = 3 * Math.random();
+    if (0 <= rand_num && rand_num < 1) {
+        return "Rock";
+    } else if (1 <= rand_num && rand_num < 2) {
+        return "Paper";
+    } else if (2 <= rand_num && rand_num < 3) {
+        return "Scissors";
+    } else {
+        return "How did we get here?";
+    }
+}
+
+console.log(GetComputerChoice());
